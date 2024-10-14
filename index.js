e02bfb09f0ef5e8daf960ec554ad52df6c953c58
Arshiajj7 <Abolfazl.jbl82@gmail.com>
2024-10-14 18:06:16 +0000

    Todo app done Commit.

---
+++ index.js
@@ -0,0 +1,50 @@
+//Add Button functionality
+const addList = document.forms["add-item"];
+const input = document.getElementById("input");
+const errorText = document.getElementById("error-text-con");
+
+addList.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const value = addList.querySelector('input[type="text"]').value;
+  if (value === "" || value == null) {
+    input.style.border = "red solid 2px";
+    input.style.boxShadow = "0 0 10px 5px #ff4747";
+    errorText.style.display = "block";
+    alert("You must write something");
+    return;
+  } else {
+    input.style.border = "0";
+    input.style.boxShadow = "0 0 10px 1px white";
+    errorText.style.display = "none";
+
+    //Creating Elements
+    const ulList = document.querySelector("ul");
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const deleteButton = document.createElement("button");
+
+    //append to DOM
+    ulList.appendChild(li);
+    li.appendChild(p);
+    li.appendChild(deleteButton);
+
+    //class list to the Elements
+    li.classList = "list-item";
+    p.textContent = value;
+    p.classList = "list-text";
+    deleteButton.textContent = "Done ✔";
+    deleteButton.classList = "delete-button";
+  }
+  input.value = "";
+});
+
+//Creating delete button for each item
+const deleteButton = document.querySelector(".box ul");
+
+deleteButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  if ((event.target.claasName = "delete-button")) {
+    const li = event.target.parentElement;
+    deleteButton.removeChild(li);
+  }
+});
